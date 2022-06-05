6fc8c8024ef4ab16777837ba938d7ba99c43114a
Nikoloz Chitashvili <chitashvili.nikoloz@gmail.com>
2022-06-05 23:38:13 +0400

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Nikoloz Chitashvili on 05.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
